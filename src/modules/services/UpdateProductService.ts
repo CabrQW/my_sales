da88b9499f825b9cbadd74d5553199948aa35ed8
CabrQW <carlosbom64@gmail.com>
2025-09-07 20:50:47 -0300

    feat: update product service

---
+++ src/modules/services/UpdateProductService.ts
@@ -0,0 +1,34 @@
+import { Product } from "@modules/database/entities/Product";
+import { productsRepositoreis } from "@modules/repositories/ProductsRepositories";
+import AppError from "@shared/errors/AppError";
+
+interface IUpdateProduct {
+  id: string,
+  name: string,
+  price: number,
+  quantity: number
+}
+
+export default class UpdateProductService {
+  async excute({ id, name, price, quantity}: IUpdateProduct): Promise<Product> {
+        const product = await productsRepositoreis.findByName(id)
+
+        if (!product) {
+          throw new AppError ("Product not found.", 404)
+        }
+
+        const productExists = await productsRepositoreis.findByName(name)
+
+        if(productExists){
+          throw new AppError('There is already one product with this name',405)
+        }
+
+        product.name = name
+        product.price = price
+        product.quantity = quantity
+
+        await productsRepositoreis.save(product)
+
+        return product
+  }
+}
