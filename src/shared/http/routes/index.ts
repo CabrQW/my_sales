8a887cc278ea7427adee5d003ad657cacea95e4b
CabrQW <carlosbom64@gmail.com>
2025-09-15 18:16:11 -0300

    feat: router updateConfig

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,7 +2,8 @@ import productsRouter from '@modules/products/routes/ProductRoutes';
 import avatarRoutes from '@modules/Users/routes/AvatarRoutes';
 import sessionsRouter from '@modules/Users/routes/SessionRoutes';
 import usersRouter from '@modules/Users/routes/UserRoutes';
-import { Router } from 'express';
+import express, { Router } from 'express';
+import updateConfig from '@config/upload'
 
 const router = Router();
 router.get('/health', (req, res) => {
@@ -13,5 +14,6 @@ router.use('/products',productsRouter )
 router.use("/users", usersRouter)
 router.use('/sessions', sessionsRouter)
 router.use('/users/avatar', avatarRoutes)
+router.use("/files", express.static(updateConfig.directory))
 
 export default router;
