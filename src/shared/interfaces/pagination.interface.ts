883375e444570e1844ac1fb39f2658037fb3b207
CabrQW <carlosbom64@gmail.com>
2025-09-19 19:38:26 -0300

    feat: pagination

---
+++ src/shared/interfaces/pagination.interface.ts
@@ -0,0 +1,9 @@
+export interface IPaginate<DateType> {
+  per_page: number;
+  total: number;
+  current_page: number;
+  total_pages: number;
+  next_page: number | null;
+  prev_page: number | null;
+  data: DateType[];
+}
